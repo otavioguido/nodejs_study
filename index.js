14ce050fb725c607c7ff007de730be61ed94b905
Otavio Guido Silva <otavio.guido-ext@ab-inbev.com>
2022-12-29 15:12:51 -0300

    add event emitter and load pages

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-const http = require(`http`);
+import { createServer } from "http";
+import { readFile } from "fs/promises";
+import { EventEmitter } from 'node:events';
 
-http.createServer((request, response) => {
+const emitter = new EventEmitter();
+
+emitter.on(`readFile`, async (response, htmlFile) => {
+    try {
+        response.end(await readFile(htmlFile, `utf-8`));
+    } catch {
+        response.statusCode = 500;
+        response.end(`Failed to load the page`);
+    }
+})
+
+createServer(async (request, response) => {
 
     const myURL = new URL(request.url, `http://${request.host}`);
-    response.writeHead(200, { 'Content-Type': 'text/html' });
+    // response.writeHead(200, { 'Content-Type': 'text/html' });    
 
     switch (myURL.pathname) {
         case `/test`:
-            response.end(`<html><body>Test page</body></html>`);
+            emitter.emit(`readFile`, response, `./pages/test.html`)
             break;
         case `/other`:
-            response.end(`<html><body>Other page</body></html>`);
+            emitter.emit(`readFile`, response, `./pages/other.html`)
             break;
         default:
-            response.end(`<html><body>Home page</body></html>`);
+            emitter.emit(`readFile`, response, `./pages/home.html`)
             break;
     }    
 }).listen(3000);
