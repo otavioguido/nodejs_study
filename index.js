bc0fda84bc50f715cced27da76cf1863ceb867a1
Otavio Guido Silva <otavio.guido-ext@ab-inbev.com>
2022-12-30 14:12:54 -0300

    finished node studying

--- index.js
+++ index.js
@@ -2,21 +2,28 @@ const http = require(`http`);
 const fs = require(`fs/promises`);
 const events = require(`node:events`);
 const mysql = require('mysql');
-
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'me',
-    password: 'secret',
-    database: 'my_db'
+    user: 'root',
+    password: 'password',
+    database: 'testDB'
 });
 
-/*
-TODO: catch path param
-TODO: manipulate request queries
-TODO: set response header
-TODO: set response body
-TODO: db manipulation
-*/
+function htmlTemplate(arg1, arg2 = ``) {
+    return `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>${arg1} page</title>
+</head>
+
+<body>
+    ${arg1} page: ${arg2}
+</body>
+
+</html>`;
+}
 
 const emitter = new events.EventEmitter();
 
@@ -30,26 +37,80 @@ emitter.on(`readFile`, async (response, htmlFile) => {
     }
 });
 
-http.createServer(async (request, response) => {
+emitter.on(`onApiFailure`, async (request, response, err) => {
+    const { headers, method, url } = request;
+    console.error(err);
+    response.writeHead(500, { 'Content-Type': 'application/json' });
+    const body = err;
+    const responseBody = { headers, method, url, body };
+    response.end(JSON.stringify(responseBody));
+});
 
-    const myURL = new URL(request.url, `http://${request.host}`);
-    connection.connect((error) => {
-        if (error) {
-            throw error;
-        }
+http.createServer(async (request, response) => {
+    const { headers, method, url } = request;
+    let body;
+    request.on('error', (err) => {
+        emitter.emit(`onApiFailure`, response, err);
+    })
+
+    response.on('error', (err) => {
+        emitter.emit(`onApiFailure`, response, err);
     });
 
-    switch (myURL.pathname) {
-        case `/test`:
-            emitter.emit(`readFile`, response, `./pages/test.html`)
-            break;
-        case `/other`:
-            emitter.emit(`readFile`, response, `./pages/other.html`)
-            break;
-        default:
-            emitter.emit(`readFile`, response, `./pages/home.html`)
-            break;
-    }
-    
-    connection.end();
+    const myURL = new URL(request.url, `http://${request.host}`);    
+
+    if (myURL.pathname.startsWith(`/path`)) {
+        const matches = myURL.pathname.match('\/path\/(.+)');
+
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        if (!matches || !matches[1]) {
+            response.end(htmlTemplate(`Path`, `Not found`));
+        } else {
+            response.end(htmlTemplate(`Path`, matches[1]));
+        }
+    } else if (myURL.pathname.startsWith(`/query`)) {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(htmlTemplate(`Query`, myURL.searchParams.get(`year`) || 0));
+    } else if (myURL.pathname.startsWith(`/post`)) {
+        if (request.method == `POST`) {
+            response.writeHead(200, { 'Content-Type': 'application/json' });
+            body = { data: 'Hello World!' };
+            const responseBody = { method, url, body };
+            response.end(JSON.stringify(responseBody));
+        }
+    } else if (myURL.pathname.startsWith(`/validate`)) {
+        body = [];
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        }).on('end', () => {
+            body = JSON.parse(Buffer.concat(body).toString());
+
+            if (body.text == `test`) {
+                console.log(`success`);
+            }
+
+            response.writeHead(200, { 'Content-Type': 'application/json' });         
+            const responseBody = { headers, method, url, body };
+            response.end(JSON.stringify(responseBody));
+        });
+    } else if (myURL.pathname.startsWith(`/save`)) {
+        body = [];
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        }).on('end', () => {
+            body = JSON.parse(Buffer.concat(body).toString());            
+
+            // connection.query(`CREATE DATABASE IF NOT EXISTS testDB;`, function (error) { if (error) throw error; });
+            // connection.query(`CREATE TABLE IF NOT EXISTS test_table(text varchar(255));`, function (error) { if (error) throw error; });
+            connection.query(`INSERT INTO test_table (text) VALUES ('${body.text}');`, function (error) { if (error) throw error; });
+
+            response.writeHead(200, { 'Content-Type': 'application/json' });
+            const responseBody = { headers, method, url, body };
+            response.end(JSON.stringify(responseBody));
+        });        
+    } else {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(htmlTemplate(`Home`));
+    }    
+
 }).listen(3000);
